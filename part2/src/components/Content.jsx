9d8f8e0699bce8f0d1318d66fe32abb8c3b0257f
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2024-07-29 12:23:01 -0400

    2.2: Course information step 7

--- part2/src/components/Content.jsx
+++ part2/src/components/Content.jsx
@@ -1,13 +1,15 @@
 import Part from "./Part ";
 
-export default function Content({ parts}) {
+export default function Content({ parts }) {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  
     return (
-        <>
-            {
-                parts.map(part => (
-                    <Part key={part.id} part={part.name} exercise={part.exercises} />
-                ))
-            }
-        </>
-    )
-}
+      <>
+        {parts.map(part => (
+          <Part key={part.id} part={part.name} exercise={part.exercises} />
+        ))}
+        <p> <strong>total of  {total} exercises</strong></p>
+      </>
+    );
+  }
+  
