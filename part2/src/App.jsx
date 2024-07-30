ec01eb3e4066f8ee826136893cf0fd113789a6d0
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2024-07-30 04:13:40 -0400

    2.12: The Phonebook step 7

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+import phoneBookService from './services/phoneBook'
 import Filter from './componentsPhonebookStep5/Filter'
 import PersonForm from './componentsPhonebookStep5/PersonForm'
 import Persons from './componentsPhonebookStep5/Persons'
@@ -17,15 +18,30 @@ const App = () => {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    phoneBookService
+      .getAll()
+      .then(initialData => {
+        setPersons(initialData)
       })
   }, [])
+  /*
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+*/
   const addContact = (event) => {
     event.preventDefault()
 
@@ -39,10 +55,15 @@ const App = () => {
       phone: newPhone
     }
 
-    setPersons(persons.concat(noteObject))
-    setNewName('')
-    setNewPhone("")
+    phoneBookService
+      .create(noteObject)
+      .then(returnedNote => {
+        setPersons(persons.concat(returnedNote))
+        setNewName('')
+        setNewPhone("")
+      })
   }
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -63,10 +84,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter search={search} handleSearchChange={handleSearchChange}/>
+      <Filter search={search} handleSearchChange={handleSearchChange} />
       <h2>Add a new</h2>
       <PersonForm newName={newName} newPhone={newPhone} handleNameChange={handleNameChange}
-      handlePhoneChange={handlePhoneChange} addContact={addContact}
+        handlePhoneChange={handlePhoneChange} addContact={addContact}
       />
       <h2>Numbers</h2>
       <Persons filteredPersons={filteredPersons} />
